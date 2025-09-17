5853ff48e10e3307346091211b8c924be88549ac
Allan Auezkhan <allanauezkhan@MacBook-Air-Allan.local>
2025-09-17 12:05:47 +0500

    first commit

---
+++ layout/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  layout
+//
+//  Created by Allan Auezkhan on 17.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+       
+       
+        view.backgroundColor = . white
+        let label = UILabel ()
+        
+        view.addSubview(label)
+        
+        label.text = "Hello world 2"
+        
+        label.font = .systemFont(ofSize: 20)
+        
+        label.frame = CGRect(x: 40, y: 200, width: 313, height: 24)
+        
+        label.textAlignment = . center
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+        constant: 150).isActive = true
+        
+        label.leadingAnchor.constraint(equalTo:
+        view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+        
+        view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo:
+        label.trailingAnchor, constant: 40) .isActive = true
+    }
+
+
+}
+
---
+++ layoutTests/layoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  layoutTests.swift
+//  layoutTests
+//
+//  Created by Allan Auezkhan on 17.09.2025.
+//
+
+import Testing
+@testable import layout
+
+struct layoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
